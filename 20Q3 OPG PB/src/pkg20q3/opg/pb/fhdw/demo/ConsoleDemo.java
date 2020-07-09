165b812a65825d3b6a8e7386a76acd9dc14ed8d5
WDHF <fwortmann@me.com>
2020-07-09 15:08:17 +0200

    Console

---
+++ 20Q3 OPG PB/src/pkg20q3/opg/pb/fhdw/demo/ConsoleDemo.java
@@ -0,0 +1,14 @@
+package pkg20q3.opg.pb.fhdw.demo;
+
+import pkg20q3.opg.pb.fhdw.utilities.Console;
+
+
+public class ConsoleDemo {
+    
+    public static void main(String[] args){
+        
+        Console.printlnMessage("Mein Name ist Florian Wortmann");
+        
+    }
+    
+}
