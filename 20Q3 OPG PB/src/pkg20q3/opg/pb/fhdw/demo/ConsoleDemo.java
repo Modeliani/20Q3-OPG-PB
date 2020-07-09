f625ed5af1d85a6a95fd2775eef6391fa1c3afee
WDHF <fwortmann@me.com>
2020-07-09 15:19:20 +0200

    ConsoleApp
    Methode erweitert

--- 20Q3 OPG PB/src/pkg20q3/opg/pb/fhdw/demo/ConsoleDemo.java
+++ 20Q3 OPG PB/src/pkg20q3/opg/pb/fhdw/demo/ConsoleDemo.java
@@ -8,7 +8,23 @@ public class ConsoleDemo {
     public static void main(String[] args){
         
         Console.printlnMessage("Mein Name ist Florian Wortmann");
+        Console.printMessage(" Mein Frau heißt ");
+        Console.printlnMessage("Anna");
+        Console.printlnErrorMessage("Hier hat was nicht geklappt");
         
+        Console.startDialogueSection();
+        int number = Console.readInt("Bitte eine Zahl eingeben");
+        Console.printlnMessage("Die Zahl lautet: " + number);
+        Console.concludeDialogueSection();
+        
+        Console.startDialogueSection();
+        String string = Console.readString("Bitte Text eingeben");
+        Console.printlnMessage("Der eingelesene Text war: " + string);
+        Console.concludeDialogueSection();
+        
+        Console.startDialogueSection();
+        String choice = Console.readChoice("Wahl 1", "Wahl 2", "Wahl 3", "Wahl 4");
+        Console.printlnMessage("Ihre Wahl war:" + choice);
     }
     
 }
