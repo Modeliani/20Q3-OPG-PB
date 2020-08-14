88e174e69714ad75b2173f0a486a99f84ca84373
WDHF <fwortmann@me.com>
2020-08-14 10:33:35 +0200

    BigDecimal runden

--- 20Q3 OPG PB/src/pkg20q3/opg/pb/fhdw/demo/BigDecimalDemo.java
+++ 20Q3 OPG PB/src/pkg20q3/opg/pb/fhdw/demo/BigDecimalDemo.java
@@ -3,20 +3,18 @@ package pkg20q3.opg.pb.fhdw.demo;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.math.RoundingMode;
 
 
 public class BigDecimalDemo {
     
     public static void main(String[] args){
         BigDecimal facor1 = new BigDecimal("1236578023576.90452876");
-        BigDecimal facor2 = new BigDecimal("123657802357634534.934534045876");
+        BigDecimal facor2 = new BigDecimal("123657802357634534.134534045876");
         
         BigDecimal product = facor1.multiply(facor2);
         System.out.println(product);
-        
-        //System.out.println(product.round(MathContext.UNLIMITED));
-        
-        
+        System.out.println(product.round(new MathContext(product.toBigInteger().toString().length(), RoundingMode.HALF_UP)));
     }
     
 }
