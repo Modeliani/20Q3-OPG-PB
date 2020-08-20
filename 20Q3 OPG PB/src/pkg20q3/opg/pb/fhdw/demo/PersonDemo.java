ba1a4498c111d4a9666b79f71aecd429c9f042e6
WDHF <fwortmann@me.com>
2020-08-20 16:14:50 +0200

    casting

--- 20Q3 OPG PB/src/pkg20q3/opg/pb/fhdw/demo/PersonDemo.java
+++ 20Q3 OPG PB/src/pkg20q3/opg/pb/fhdw/demo/PersonDemo.java
@@ -8,5 +8,19 @@ public class PersonDemo {
     public static void main(String[] args){
         Person person = new Person("Heinz", new Location("Street", 2345, "Stadt"));
         System.out.println(person.toString());
+        Lecturer doz1 = new Lecturer("Frank", new Location("Strasse 1", 2345, "Stadt"), Department.COMPUTER_SCIENCE, Role.VISITING_LECTURER);
+        System.out.println(doz1.toString());
+        Student student = new Student("Peter", new Location("Strasse 199", 2345, "Stadt"), Subject.BUSNESS_COMPUTER_SCIENCE);
+        System.out.println(student.toString());
+        
+        person = student; // Polymorpie
+        System.out.println(person); // Late Binding
+        //person = new Lecturer("Franz", new Location("Hauptstrasse 1234", 43245, "Altstadt"), Department.BUSINESS_SCIENDE, Role.PROFESSOR_ONLY);
+        //System.out.println(person); // Late Binding
+        
+        if (person instanceof Student){ // Typabfrage mit instanceof
+            student = (Student) person; //Cast
+        }
+        System.out.println(student);
     }
 }
--- 20Q3 OPG PB/src/pkg20q3/opg/pb/fhdw/model/Thing.java
+++ 20Q3 OPG PB/src/pkg20q3/opg/pb/fhdw/model/Thing.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pkg20q3.opg.pb.fhdw.model;
 
-/**
- *
- * @author Florian
- */
-public class Thing {
+
+/*final*/ public class Thing {
+    
+    protected String name;
+    private int id;
+    
+    public Thing(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+    
+    /*final*/ public String getName(){
+        return name;
+    }
     
 }
